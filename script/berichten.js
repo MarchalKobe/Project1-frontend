09c8792daf60c1176ca71eb0fdfc946e4dc9cad0
MarchalKobe <kobe.marchal@student.howest.be>
2020-06-08 19:06:31 +0200

    Update messages

--- script/berichten.js
+++ script/berichten.js
@@ -1,5 +1,6 @@
 //#region ***  DOM references ***
-let html_nickname, html_answer, html_send_button, html_message;
+let html_nickname, html_answer, html_send_button, html_message, html_answer_text;
+let messageID, messageInterval;
 //#endregion
 
 //#region ***  Callback-Visualisation - show___ ***
@@ -11,11 +12,28 @@ const showNickname = function(jsonObject) {
 }
 
 const showSendMessage = function(jsonObject) {
-    console.log(jsonObject);
+    html_answer = document.querySelector(".js-answer");
+
+    html_answer.style.display = "inherit";
+    messageID = jsonObject.id;
+    console.log(messageID);
+    
+    messageInterval = setInterval(getAnswer, 500);
 };
 
 const showSendError = function(jsonObject) {
     console.log(jsonObject);
+};
+
+const showAnswer = function(jsonObject) {
+    html_answer_text = document.querySelector(".js-answer-text");
+
+    let answer = jsonObject.answer.Antwoord;
+    if(answer) {
+        console.log(answer);
+        clearInterval(messageInterval);
+        html_answer_text.innerHTML = answer;
+    }
 };
 //#endregion
 
@@ -30,6 +48,14 @@ const getNickname = function() {
         handleData("http://192.168.0.120:5000/api/v1/nickname", showNickname, null, "GET", null, token);
     };
 };
+
+const getAnswer = function() {
+    const token = sessionStorage.getItem("token");
+
+    if(token) {
+        handleData(`http://192.168.0.120:5000/api/v1/message/answer/${messageID}`, showAnswer, null, "GET", null, token);
+    };
+};
 //#endregion
 
 //#region ***  Event Listeners - listenTo___ ***
@@ -37,10 +63,13 @@ const listenToSendButton = function() {
     html_answer = document.querySelector(".js-answer");
     html_send_button = document.querySelector(".js-send-button");
     html_message = document.querySelector(".js-message");
+    html_answer_text = document.querySelector(".js-answer-text");
 
-    html_answer.innerHTML = "";
+    html_answer.style.display = "none";
 
     html_send_button.addEventListener("click", function() {
+        html_answer_text.innerHTML = "Wachten..";
+
         const message = html_message.value;
         const data = {
             message: message
